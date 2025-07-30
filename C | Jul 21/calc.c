2fc67f0c159e218b9eabc02e8aafdcf897046496
Gurgen Ayvazyan <ayvazyangurgen777@gmail.com>
2025-07-30 15:15:06 +0400

    Update calc.c
    last version

--- C | Jul 21/calc.c
+++ C | Jul 21/calc.c
@@ -9,30 +9,29 @@
 			int num1 = 0;
 			int num2 = 0;
 			int res = 0;
-			char tmp = 0;
-			char act[1];
-			char stop[1];
-			while( stop[0] != 'Q'){
+			char act = '\0';
+			char stop = '\0';
+			while( stop != 'Q'){
 				printf("Input first number: ");
                 scanf("%d", &num1);
                 printf("Choose action ( +, -, *, /): \n");
-                scanf(" %c", &act[0]);
+                scanf(" %c", &act);
 
                 printf("Input second number: ");
                 scanf("%d", &num2);
                            
-				if (act[0] == '+'){
+				if (act == '+'){
 					printf("Res : %d\n", num1 + num2);
-				} else if (act[0] == '-'){
+				} else if (act == '-'){
 					printf("Res : %d\n", num1 - num2);
-				} else if (act[0] == '*'){
+				} else if (act == '*'){
 				    printf("Res : %d\n", num1 * num2);
-				} else if (act[0] == '/'){
+				} else if (act == '/'){
 	                  printf("Res : %d\n", num1 / num2);
 				}  
 				   
               	printf("Press Q to exit: ");
-	        scanf(" %c", &stop[0]);
+	        scanf(" %c", &stop);
 	        printf("--------end--------\n");
 				}
 		return 0; }	
